07f37a1e639dacd00262bb098183a19d044e3480
Amy Guan <amyg22@yorkhouse.ca>
2019-11-27 13:56:18 -0800

    change in camera code

--- Assets/camera.cs
+++ Assets/camera.cs
@@ -6,15 +6,29 @@ public class camera : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset;
+    public float speed;
 
     private void Start()
     {
 
         offset = transform.position - player.transform.position;
+        speed = 0.1f;
     }
-
+    // void FixedUpdate()
+    // {
+    //      if(Input.GetKey(KeyCode.Q))
+    //      {
+    //          transform.position = new Vector3(transform.position.x, transform.position.y - speed, transform.position.z);
+    //      }
+    //      if(Input.GetKey(KeyCode.E))
+    //      {
+    //          transform.position = new Vector3(transform.position.x, transform.position.y + speed, transform.position.z);
+    //      }
+    // }
         void LateUpdate()
     {
+            // if(Input.GetKey(KeyCode.Space))
             transform.position = player.transform.position + offset;
     }
+    
 }
